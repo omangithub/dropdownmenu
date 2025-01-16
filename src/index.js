90d3860c365fb4a1e8155fafce80f8302576543d
oman <smithowain@hotmail.com>
2025-01-16 22:13:37 +0900

    drop down menu positioned correctly on screen

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import "./styles.css";
 
+const dropDownMenu = (function() {
+  
+    const menuButton = document.getElementById("buttonBar");
+    const dropDownOptions = document.getElementById("optionsBox");
+    const option2 = document.getElementById("op2");
+    const option3 = document.getElementById("op3");
+
+    menuButton.addEventListener("click",()=>{
+      dropDownOptions.classList.toggle("hidden");
+      console.log("somethings happening here")
+    })
+})();
